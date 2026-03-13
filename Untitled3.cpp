da1a93d7a2918b6f27b4aca2ad9bce4b7f4eba83
25A31A05JO <bhavyasruthimunjeti@gmail.com>
2026-03-13 21:21:12 +0530

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"enter a,b,c variables";
+	cin>>a>>b>>c;
+	if((a>>b)&&(a>>b))
+	cout<<"a is big";
+	else if(b>>c)
+	cout<<"b is big";
+	else
+	cout<<"c is big";
+	return 0;
+	 
+}
